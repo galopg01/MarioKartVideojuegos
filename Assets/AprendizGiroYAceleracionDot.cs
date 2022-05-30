d8e66b9a99c5529f882cc2a70631e514ded53c8d
nicor <nicoromero07@uma.es>
2022-05-31 00:09:01 +0200

    FixedUpdated, aprendizGiroYAceleracion
    Falta afinar con la funcion de ponderacion

--- Assets/AprendizGiroYAceleracionDot.cs
+++ Assets/AprendizGiroYAceleracionDot.cs
@@ -12,16 +12,17 @@ using java.util;
 using weka.classifiers.functions;
 using weka.classifiers;
 using weka.core.converters;
+using System;
 
 public class AprendizGiroYAceleracionDot : MonoBehaviour
 {
-    weka.classifiers.trees.M5P saberPredecirGiro;
+    weka.classifiers.trees.M5P saberPredecirGiro, saberPredecirVelocidad, saberPredecirDot;
     weka.core.Instances casosEntrenamiento;
     Text texto;
     private string ESTADO = "Sin conocimiento";
     public GameObject PuntoObjetivo;
     public float valorMaximoAceleracion, valorMaximoGiro, paso = 1, Velocidad_Simulacion = 1;
-    float mejorGiro, distanciaObjetivo;
+    float mejorGiro, mejorSpeed, menorDot, mejorPonderacion=0;
     private Vector3 posicionActual;
     private Vector3 rotacionActual;
     Rigidbody r;
@@ -32,7 +33,7 @@ public class AprendizGiroYAceleracionDot : MonoBehaviour
     DotProduct script;
 
     private float DotInicial;
-    private bool esperando = true;
+    private bool esperando = false;
 
     void Start()
     {
@@ -110,7 +111,13 @@ public class AprendizGiroYAceleracionDot : MonoBehaviour
         casosEntrenamiento.setClassIndex(1);                                             //y hace que aprenda Fx dada la distancia y Fy
         saberPredecirGiro.buildClassifier(casosEntrenamiento);                        //REALIZA EL APRENDIZAJE DE FX A PARTIR DE LA DISTANCIA Y FY
 
-        distanciaObjetivo = 0;
+        saberPredecirVelocidad = new M5P();
+        casosEntrenamiento.setClassIndex(3);                                             //y hace que aprenda Fx dada la distancia y Fy
+        saberPredecirVelocidad.buildClassifier(casosEntrenamiento);                        //REALIZA EL APRENDIZAJE DE FX A PARTIR DE LA DISTANCIA Y FY
+
+        saberPredecirDot = new M5P();
+        casosEntrenamiento.setClassIndex(2);                                             //y hace que aprenda Fx dada la distancia y Fy
+        saberPredecirDot.buildClassifier(casosEntrenamiento);                        //REALIZA EL APRENDIZAJE DE FX A PARTIR DE LA DISTANCIA Y FY
 
         ESTADO = "Con conocimiento";
         print("uwu");
@@ -129,22 +136,44 @@ public class AprendizGiroYAceleracionDot : MonoBehaviour
             if (esperando && Time.time - time >= 0.5)
             {
                 Instance casoPrueba = new Instance(casosEntrenamiento.numAttributes());  //Crea un registro de experiencia durante el juego
-                casoPrueba.setDataset(casosEntrenamiento);
-                casoPrueba.setValue(0, script.dot);                               //le pone el dato de la distancia a alcanzar
-                casoPrueba.setValue(2, distanciaObjetivo);
-
-                mejorGiro = (float)saberPredecirGiro.classifyInstance(casoPrueba);  //predice la fuerza dada la distancia utilizando el algoritmo M5P
-
+                
+                for (float speed = 0; speed<=1; speed+=paso)
+                {
+                    for(float giro = -1; giro<=valorMaximoGiro; giro += paso)
+                    {
+                        casoPrueba.setDataset(casosEntrenamiento);
+                        casoPrueba.setValue(0, script.dot);   
+                        casoPrueba.setValue(1, giro);
+                        casoPrueba.setValue(3, speed);
+                        float dot = (float)saberPredecirDot.classifyInstance(casoPrueba);
+                        float ponderacion = calcularPonderacion(speed, dot);
+                        if (ponderacion>mejorPonderacion)
+                        {
+                            mejorGiro = giro;
+                            mejorSpeed = speed;
+                            mejorPonderacion = ponderacion;
+                            
+                        }
+                    }
+                    
+                }
+                
                 if (mejorGiro > -0.05 && mejorGiro < 0.05) mejorGiro = 0;
                 if (mejorGiro < -1) mejorGiro = -1;
                 if (mejorGiro > 1) mejorGiro = 1;
 
                 wheelController.turn = mejorGiro;
+                wheelController.speed = mejorSpeed;
 
-                print("DECISION REALIZADA: Se avanza con giro =" + mejorGiro + "para dot = " + script.dot);
+                print("DECISION REALIZADA: Se avanza con giro =" + mejorGiro + "y velocidad = "+ mejorSpeed + "para dot = " + script.dot);
                 esperando = false;
             }
         }
         
+    }
+
+    private float calcularPonderacion(float speed, float dot)
+    {
+        return (float)(( (speed*80) + (1 / System.Math.Pow(10, 12)*dot)) /100);
     }
 }
